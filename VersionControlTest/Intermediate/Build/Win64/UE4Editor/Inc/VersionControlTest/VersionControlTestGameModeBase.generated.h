53401a4e9831a687f590701798f07c1463a413b9
ArshPervezGithub <arsh.pervez@mikutech.com>
2021-05-10 12:53:59 -0400

    BOX-141 Create Test Project

---
+++ VersionControlTest/Intermediate/Build/Win64/UE4Editor/Inc/VersionControlTest/VersionControlTestGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VERSIONCONTROLTEST_VersionControlTestGameModeBase_generated_h
+#error "VersionControlTestGameModeBase.generated.h already included, missing '#pragma once' in VersionControlTestGameModeBase.h"
+#endif
+#define VERSIONCONTROLTEST_VersionControlTestGameModeBase_generated_h
+
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_SPARSE_DATA
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_RPC_WRAPPERS
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVersionControlTestGameModeBase(); \
+	friend struct Z_Construct_UClass_AVersionControlTestGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AVersionControlTestGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/VersionControlTest"), NO_API) \
+	DECLARE_SERIALIZER(AVersionControlTestGameModeBase)
+
+
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAVersionControlTestGameModeBase(); \
+	friend struct Z_Construct_UClass_AVersionControlTestGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AVersionControlTestGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/VersionControlTest"), NO_API) \
+	DECLARE_SERIALIZER(AVersionControlTestGameModeBase)
+
+
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVersionControlTestGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVersionControlTestGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVersionControlTestGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVersionControlTestGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVersionControlTestGameModeBase(AVersionControlTestGameModeBase&&); \
+	NO_API AVersionControlTestGameModeBase(const AVersionControlTestGameModeBase&); \
+public:
+
+
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVersionControlTestGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVersionControlTestGameModeBase(AVersionControlTestGameModeBase&&); \
+	NO_API AVersionControlTestGameModeBase(const AVersionControlTestGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVersionControlTestGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVersionControlTestGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVersionControlTestGameModeBase)
+
+
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_12_PROLOG
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_SPARSE_DATA \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_RPC_WRAPPERS \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_INCLASS \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_SPARSE_DATA \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VERSIONCONTROLTEST_API UClass* StaticClass<class AVersionControlTestGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID VersionControlTest_Source_VersionControlTest_VersionControlTestGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ VersionControlTest/Intermediate/Build/Win64/VersionControlTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.27.29112
+#include "G:/Arsh/Unreal/Learning/Advanced/VersionControlTest/Intermediate/Build/Win64/VersionControlTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
